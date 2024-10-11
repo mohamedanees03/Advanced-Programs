4e76a7fef2432a2babe59c29d2668a15e7a6ed61
mohamedanees03 <aneesvms1920@gmail.com>
2024-10-11 20:55:38 +0530

    Added new code

---
+++ MoveZeros.java
@@ -0,0 +1,21 @@
+public class MoveZeros {
+    public static void main(String[] args) {
+        int[] nums={0,1,0,3,12};
+        moveTheZeros(nums);
+    }
+    public static void moveTheZeros(int[] nums){
+        int count=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0)
+                nums[count++]=nums[i];
+        }
+
+        while(count<nums.length){
+            nums[count++]=0;
+        }
+
+        for(int i : nums) {
+            System.out.print(i+" ");
+        }
+    }
+}
